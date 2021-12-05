7c1df3428292b1b727b26bd2afd2cd78311cfaae
Ozioma <oziomaokoroafor@gmail.com>
2021-12-05 20:25:07 +0100

    sorted squares commit

--- algoExpertProblems/sorted_squared_array.py
+++ algoExpertProblems/sorted_squared_array.py
@@ -8,14 +8,30 @@ def sort_and_square_array(array):
     return new_array
 
 
+# reversed function in python. It simply reverses lists and the likes
+def sorting_and_squaring_arrays(array):
+    array.sort()
+    sorted_squares_array = [0 for _ in array]
+    smallest_value_index = 0
+    largest_value_index = len(array) - 1
+
+    for index_in_descending_order in reversed(range(len(array))):
+
+        smallest_value = array[smallest_value_index]
+        largest_value = array[largest_value_index]
+
+        if abs(smallest_value) > abs(largest_value):
+            sorted_squares_array[index_in_descending_order] = smallest_value * smallest_value
+            smallest_value_index += 1
+
+        else:
+            sorted_squares_array[index_in_descending_order] = largest_value * largest_value
+            largest_value_index -= 1
+    return sorted_squares_array
+
+
 first_array = [4, 3, 6, 8, 2]
 
 print(sort_and_square_array(first_array))
 
-
-# function of the reversed function in python
-def sorting_and_squaring_arrays(array):
-    print(list(reversed(range(len(array)))))
-
-
-sorting_and_squaring_arrays(first_array)
+print(sorting_and_squaring_arrays(first_array))
