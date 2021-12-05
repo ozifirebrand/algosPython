99ed978d2de7a9a1be5896feb1578fbe15a28118
Ozioma <oziomaokoroafor@gmail.com>
2021-12-05 19:59:08 +0100

    reversed function test

--- algoExpertProblems/sorted_squared_array.py
+++ algoExpertProblems/sorted_squared_array.py
@@ -1,3 +1,5 @@
+# space and time complexity This is an O(nlog n) time algorithm and an O(n) space
+# Not efficient enough
 def sort_and_square_array(array):
     new_array = []
     for value in array:
@@ -9,3 +11,11 @@ def sort_and_square_array(array):
 first_array = [4, 3, 6, 8, 2]
 
 print(sort_and_square_array(first_array))
+
+
+# function of the reversed function in python
+def sorting_and_squaring_arrays(array):
+    print(list(reversed(range(len(array)))))
+
+
+sorting_and_squaring_arrays(first_array)
