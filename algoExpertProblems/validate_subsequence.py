687a9937bb4242a17d905725e24f8968beaf2abc
Ozioma <oziomaokoroafor@gmail.com>
2021-12-05 17:19:52 +0100

    Validate sequence

--- algoExpertProblems/validate_subsequence.py
+++ algoExpertProblems/validate_subsequence.py
@@ -1,10 +1,15 @@
 def is_valid_subsequence(array, sequence):
-    array_index = 0
+    # array_index = 0
     sequence_index = 0
-    while sequence_index < len(sequence) and array_index < len(array):
-        if array[array_index] == sequence[sequence_index]:
-            sequence_index += 1
-        array_index += 1
+    # while sequence_index < len(sequence) and array_index < len(array):
+    #     if array[array_index] == sequence[sequence_index]:
+    #         sequence_index += 1
+    #     array_index += 1
+    for item in array:
+        for item2 in array:
+            if item2 == item:
+                sequence_index += 1
+            else : continue
     return sequence_index == len(sequence)
 
 
