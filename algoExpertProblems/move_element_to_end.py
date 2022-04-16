ab3ce326a314daffea5b6e887e36cec42d23fd6c
Ozioma <oziomaokoroafor@gmail.com>
2022-04-16 17:24:22 +0100

    Move element

---
+++ algoExpertProblems/move_element_to_end.py
@@ -0,0 +1,28 @@
+def moveElementToEnd(array, toMove):
+    # Write your code here.
+    # initialise two pointers, left and right including an index
+    # initialise values to array with pointers as indices
+    # while left pointer is lesser than or equal to right pointer plus one
+    # for right pointer, if value is equal to to-move value, reduce pointer by one
+    # for left pointer if value is equal to to move value, and right pointer not equals to move
+    # value, swap
+    # then increment left pointer by one
+    # return array
+    left = 0
+    right = len(array) - 1
+
+    while len(array) != 0 and left != right:
+        if array[right] == toMove:
+            right -= 1
+        if array[left] == toMove and array[right] != toMove:
+            save = array[left]
+            array[left] = array[right]
+            array[right] = save
+            left += 1
+        if array[left] != toMove:
+            left += 1
+    return array
+
+
+array = [2, 1, 2, 2, 2, 3, 4, 2]
+print(moveElementToEnd(array, 2))
